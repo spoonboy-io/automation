35e35b378536b3a2fa7590458d2d1cafc76e19ad
Ollie Phillips <ollie@interject.io>
2023-06-13 08:52:29 +0100

    pre-test code

--- python/update_node_types/when_deleted_70729/update.py
+++ python/update_node_types/when_deleted_70729/update.py
@@ -17,6 +17,7 @@ headers = {
 cacheFile = "azureNodeTypeCache.json" ## dev
 ##cacheFile = "/tmp/azureNodeTypeCache.json" ## morpheus
 hasCacheVersion = False
+cacheNodeTypes = []
 
 getImages = "https://%s/api/virtual-images?max=1000&filterType=Synced&imageType=vhd" % (host)
 getNodeTypes = "https://%s/api/library/container-types?max=2000&provisionType=azure&sort=id&direction=asc" % (host)
@@ -46,17 +47,42 @@ for nt in nodeTypes:
 
         ## if no cache exists and we have nulls there's nothing to do, but log that the nulls have been found
         ## and that the node types will have to be manually updated
-        if !hasCacheVersion:
-            print("something to the above effect")
+        if hasCacheVersion:
+
+            ## lookup the node in the cached data
+            for nc in cacheNodeTypes:
+                if nc["id"] == nt["id"]:
+                    ## get the virtual image that was attached
+                    vi = nt["virtualImage"]
+                    if vi is not None:
+                        ## get then update the node type with virtual image
+                        apiNodeType = "https://%s/api/library/container-types/%s" % (host, nt["id"])
+                        res = requests.get(apiNodeType, headers=headers, verify=False)
+                        nodeType = res.json()
+
+                        ## update the info
+                        nodeType["containerType"]["virtualImage"]["id"] = vi["id"]
+                        nodeType["containerType"]["virtualImage"]["name"] = vi["name"]
+
+                        putData = json.dumps(nodeType)
+
+                        ## make the put request with data
+                        res = requests.put(apiNodeType, data=putData, headers=headers, verify=False)
+                        msg = ""
+                        if res.status_code == 200:
+                            msg = "Response: %s, Node type: %s (id: %s) was relinked to virtual image; %s (id: %s)" % (res.status_code, nt["name"], nt["id"], vi["name"], vi["id"])
+                        else:
+                            msg = "ErrResponse: %s, Node type: %s (id: %s) virtual image will need to be added manually" % (res.status_code, nt["name"], nt["id"])
+                        print(msg)
         else:
-            ## can we look up the equivalent virtual image in morpheus?
-            ## we will have name, version, and we can look for later ID version for extra valids
-            ## if we can find it, and be certain, update the node type with new virtual image data
+            ## nothing to be done output some information about the node type
+            msg = "Node type: %s (id: %s) is disconnected from a virtual image, there is no cache so a virtual image will need to be added manually" % (nt["name"], nt["id"])
+            print(msg)
 
-
+## write or update cache conditionally
 if writeCache:
-    ## write or update cache
     f = open(cacheFile, "w")
     f.write(json.dumps(nodeTypes))
     f.close()
-
+    msg = "node types cached for tracking in file: %s" % cacheFile
+    print(msg)
