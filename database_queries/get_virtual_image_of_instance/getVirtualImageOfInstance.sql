2627eec44523cfe2f404014835d10610a78e3a6a
Ollie Phillips <ollie@interject.io>
2023-06-15 18:45:26 +0100

    tidy up

--- database_queries/get_virtual_image_of_instance/getVirtualImageOfInstance.sql
+++ database_queries/get_virtual_image_of_instance/getVirtualImageOfInstance.sql
@@ -1,5 +1,5 @@
 /*
-Obtain the virtual image from instance
+obtain the virtual image of an instance
 */
 
-select i.id as instanceId, i.name as instanceName, vi.id as imageId, vi.name as imageName from instance i inner join instance_type_layout_container_type_set ic inner join container_type_set cts inner join container_type ct inner join virtual_image vi where i.id = 2 and i.layout_id = ic.instance_type_layout_containers_id and ic.container_type_set_id = cts.id and cts.container_type_id = ct.id and ct.virtual_image_id = vi.id;
+select i.id as instanceId, i.name as instanceName, vi.id as imageId, vi.name as imageName from instance i inner join instance_type_layout_container_type_set ic inner join container_type_set cts inner join container_type ct inner join virtual_image vi where i.layout_id = ic.instance_type_layout_containers_id and ic.container_type_set_id = cts.id and cts.container_type_id = ct.id and ct.virtual_image_id = vi.id and i.id = 2;
