0a0bcb7f8268858b66ad329425d3dbebcb69be8e
Ollie Phillips <ollie@interject.io>
2023-06-15 18:35:59 +0100

    add a section for sql

---
+++ database_queries/get_virtual_image_of_instance/getVirtualImageOfInstance.sql
@@ -0,0 +1,5 @@
+/*
+Obtain the virtual image from instance
+*/
+
+select i.id as instanceId, i.name as instanceName, vi.id as imageId, vi.name as imageName from instance i inner join instance_type_layout_container_type_set ic inner join container_type_set cts inner join container_type ct inner join virtual_image vi where i.id = 2 and i.layout_id = ic.instance_type_layout_containers_id and ic.container_type_set_id = cts.id and cts.container_type_id = ct.id and ct.virtual_image_id = vi.id;
