d0b0653a871d1a0d241a90cc94987bf5788ee57c
Ollie Phillips <ollie@interject.io>
2023-07-11 17:08:54 +0100

    handle new users not yet logged out via union query

--- database_queries/capture_logged_in_users/captureLoggedInUsers.sql
+++ database_queries/capture_logged_in_users/captureLoggedInUsers.sql
@@ -1,16 +1,16 @@
 /*
-
 This query compares the last login date of users with their last log out date, if the former is greater than the latter
 then the user is currently logged in.
 
-There is one caveat.  It will not capture first login of users because there is no log out date to compare against.
+The union query also allows it to capture new users who have logged in, but not yet logged out.
 
-There is also an `in` exclusion list which contains a list of ids to ignore as they appear logged in but are not.
-I just had the user to ignore and in the query below,
-
+Computes session_mins
 */
 
-select login.user_id, in_date, out_date from (select user_id, max(date_created) as in_date from audit_log
-    where event_type = 'login#process' and user_id is not NULL group by user_id) as login, (select user_id, max(date_created) as out_date from audit_log
-        where event_type = 'logout#index' and user_id is not NULL group by user_id) as logout where login.user_id = logout.user_id and in_date > out_date
-            and login.user_id not in (3);
+select login.user_id, in_date, TIMESTAMPDIFF(MINUTE, in_date, now()) as session_mins from (select user_id, max(date_created) as in_date from audit_log where event_type = 'login#process'
+    and user_id is not NULL group by user_id) as login inner join (select user_id, max(date_created) as out_date from audit_log where event_type = 'logout#index' and user_id is not NULL group by user_id) as logout
+        on login.user_id = logout.user_id where in_date > out_date
+union(
+    select login.user_id, in_date, TIMESTAMPDIFF(MINUTE, in_date, now()) as session_mins from (select user_id, max(date_created) as in_date from audit_log where event_type = 'login#process'
+        and user_id is not NULL group by user_id) as login where  login.user_id not in (select user_id from audit_log where event_type = 'logout#index' and user_id is not NULL group by user_id)
+);
